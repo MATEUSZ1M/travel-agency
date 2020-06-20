eab4dc25b19158aaba53169dd8fbff93e4b51691
Mateusz Mikolajczyk <mikolajczykmateusz1993@gmail.com>
2020-06-20 21:42:08 +0000

    Add update time funcionality

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -1,32 +1,43 @@
 import React from 'react';
 import styles from './HappyHourAd.scss';
-import PropTypes  from 'prop-types';
+import PropTypes from 'prop-types';
 
 class HappyHourAd extends React.Component {
-  
   static propTypes = {
     title: PropTypes.string,
-  }
+  };
 
-  constructor(){
+  constructor() {
     super();
-  
+
+    const period = 1000;
     /* run this.forceUpdate() every second */
-    this.forceUpdate();
+    setInterval(() => {
+      this.forceUpdate();
+    }, period);
   }
 
-  getCountdownTime(){
+  getCountdownTime() {
     const currentTime = new Date();
-    const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));
-  
-    if(currentTime.getUTCHours() >= 12){
-      nextNoon.setUTCDate(currentTime.getUTCDate()+1);
+    const nextNoon = new Date(
+      Date.UTC(
+        currentTime.getUTCFullYear(),
+        currentTime.getUTCMonth(),
+        currentTime.getUTCDate(),
+        12,
+        0,
+        0,
+        0
+      )
+    );
+
+    if (currentTime.getUTCHours() >= 12) {
+      nextNoon.setUTCDate(currentTime.getUTCDate() + 1);
     }
-  
-    return Math.round((nextNoon.getTime() - currentTime.getTime())/1000);
+
+    return Math.round((nextNoon.getTime() - currentTime.getTime()) / 1000);
   }
 
-  
   render() {
     const { title } = this.props;
 
